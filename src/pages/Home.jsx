58ee3bd9c7d17029a03f83fc64c5d6f96c6d1cbf
islam-nazrul <islamnazrul8408@gmail.com>
2026-05-30 01:17:01 +0530

    add initial files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,19 @@
 import Hero from '../components/Hero'
-  import Speciality from '../components/Speciality'
-  import DoctorsSection from '../components/DoctorsSection'
-  import BookingBanner from '../components/BookingBanner'
-  import WhyChooseUs from '../components/WhyChooseUs'
+import Speciality from '../components/Speciality'
+import DoctorsSection from '../components/DoctorsSection'
+import BookingBanner from '../components/BookingBanner'
+import WhyChooseUs from '../components/WhyChooseUs'
   
-  function Home() {
-    return (
-      <div>
-        <Hero />
-        <Speciality />
-        <DoctorsSection />
-        <BookingBanner />
-        <WhyChooseUs />
-      </div>
-    )
-  }
+function Home() {
+  return (
+    <div>
+      <Hero />
+      <Speciality />
+      <DoctorsSection />
+      <BookingBanner />
+      <WhyChooseUs />
+    </div>
+  )
+}
   
-  export default Home
+export default Home
