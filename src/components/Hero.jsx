2dfc8d60de5068f9d1114bde3e438e132a40d0f6
islam-nazrul <islamnazrul8408@gmail.com>
2026-05-31 16:27:06 +0530

    Update hero button and banner animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,42 @@
 import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 function Hero() {
+  const MotionLink = motion(Link);
+  
   return (
     <section className="bg-blue-50">
       <div className="mx-auto px-6 grid gap-10 items-center hero-banner-main-contnr">
         <div className="hero-overlay">
           <div className="hero-text-contnr">
-            <h1 className="text-5xl font-bold text-gray-800 leading-tight">
-              Book Appointment With Trusted Doctors
-            </h1>
-
-            <p className="mt-6 text-gray-600 text-lg">
-              Professional healthcare services for you and your family.
-            </p>
-
-            <Link
-              to="/appointment"
-              className="inline-block mt-6 bg-blue-600 text-white px-6 py-3 rounded-full shadow hover:bg-blue-700 transition"
+            <motion.h1 className="text-5xl font-bold text-gray-800 leading-tight"
+              initial={{ opacity: 0, y: 80 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8 }}
             >
-              Book Appointment
-            </Link>
+              Book Appointment With Trusted Doctors
+            </motion.h1>
+
+            <motion.p className="mt-6 text-gray-600 text-sm"
+              initial={{ opacity: 0, y: 80 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, delay: 0.2 }}
+            >
+              Book Appointment With Trusted Doctors
+            </motion.p>
+
+            <motion.div
+              initial={{ opacity: 0, y: 80 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, delay: 0.4 }}
+            >
+              <MotionLink
+                to="/appointment"
+                className="inline-block border border-white text-white px-6 py-2 rounded-full hover:bg-white hover:text-blue-600 transition duration-300 mt-8 cursor-pointer rounded-full shadow hover:bg-blue-700 transition"
+              >
+                Book Appointment
+              </MotionLink>
+            </motion.div>
           </div>
         </div>
         <div className="hero-banner">
